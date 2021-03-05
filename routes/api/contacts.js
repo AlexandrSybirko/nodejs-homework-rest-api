7865d73b63a9048f426cf47b2ee0fa85e38e3425
AlexandrSybirko <63520677+AlexandrSybirko@users.noreply.github.com>
2021-03-05 16:49:25 +0200

    adds controller

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,105 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const Contacts = require('../../model/contacts')
+const contactsController = require('../../controllers/contacts')
 
-router.get('/', async (req, res, next) => {
-  try {
-    const contacts = await Contacts.listContacts()
-    return res.json({
-      status: 'success',
-      code: 200,
-      data: {
-        contacts,
-      },
-    })
-  } catch (e) {
-    next(e)
-  }
-})
+router
+  .get('/', contactsController.getAll)
+  .post('/', contactsController.create)
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const contact = await Contacts.getContactById(req.params.contactId)
-    if (contact) {
-      return res.json({
-        status: 'success',
-        code: 200,
-        data: {
-          contact,
-        },
-      })
-    } else {
-      return res.status(404).json({
-        status: 'error',
-        code: 404,
-        data: 'Not Found',
-      })
-    }
-  } catch (e) {
-    next(e)
-  }
-})
-
-router.post('/', async (req, res, next) => {
-  try {
-    const contact = await Contacts.addContact(req.body)
-    return res.status(201).json({
-      status: 'success',
-      code: 201,
-      data: {
-        contact,
-      },
-    })
-  } catch (e) {
-    next(e)
-  }
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const contact = await Contacts.removeContact(req.params.contactId)
-    if (contact) {
-      return res.json({
-        status: 'success',
-        code: 200,
-        data: {
-          contact,
-        },
-      })
-    } else {
-      return res.status(404).json({
-        status: 'error',
-        code: 404,
-        data: 'Not Found',
-      })
-    }
-  } catch (e) {
-    next(e)
-  }
-})
-
-router.patch('/:contactId', async (req, res, next) => {
-  try {
-    console.log(req.body)
-    const contact = await Contacts.updateContact(req.params.id, req.body)
-    if (contact) {
-      return res.json({
-        status: 'success',
-        code: 200,
-        data: {
-          contact,
-        },
-      })
-    } else {
-      return res.status(404).json({
-        status: 'error',
-        code: 404,
-        data: 'Not Found',
-      })
-    }
-  } catch (e) {
-    next(e)
-  }
-})
+router
+  .get('/:contactId', contactsController.getById)
+  .delete('/:contactId', contactsController.remove)
+  .patch('/:contactId', contactsController.update)
 
 module.exports = router
